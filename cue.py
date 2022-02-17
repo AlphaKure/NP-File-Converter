0277f20d444944b6101f641c2f07399092c983ec
Alpha <alpha6662017@outlook.com>
2022-02-17 19:54:40 +0800

    [cue] cueFile解密自動化完成

--- cue.py
+++ cue.py
@@ -1,47 +1,60 @@
 import os
 import ujson
-
+import shutil
 
 def cue(path:str):
 
-
-    #讀取設定 deretore路徑
+    #讀取設定 
     with open('setting.json','r',encoding='utf-8')as f:
         setting=ujson.load(f)
         f.close()
     deretore=setting['deretore']
+    critool=setting['critool']
+    key=setting['key']
+    sat=setting['sat']
 
     #檢查path
     if os.path.isdir(path):
         if not path.endswith('\\'):
             path=path+'\\'
-        filelist=os.listdir(path)
-        
-        #檢查path內非wav檔(保險用)
-        for file in filelist:
-            if not file.endswith('.wav'):
-                filelist.remove(file)
-        filecount=len(filelist)
+        dirlist=os.listdir(path)
 
-        #重新編號命名
-        for i in range(0,filecount):
-            nowfile=path+filelist[i]
-            if i<=9:
-                newname=path+'0000'+str(i)+'_streaming.wav'
-            else:
-                newname=path+'000'+str(i)+'_streaming.wav'
-            os.rename(nowfile,newname)
-            os.system(f'{deretore} {newname}')
-        
-        #刪除轉換前檔案
-        filelist=os.listdir(path)
-        for file in filelist:
-            if file.endswith('.wav'):
-                os.remove(path+file)
-        print('[SUCCESS] All Done!')
+        #讀取acb檔並解密
+        for dir in dirlist:
+            filelist=os.listdir(path+dir)
+            for file in filelist:
+                if file.endswith('.acb'):
+                    target_acb=path+dir+'\\'+file
+                    tmpdir=(path+dir+'\\'+file).replace('.acb','\\')
+            os.system(f'node {critool}index.js acb2wavs -k {key} {target_acb} ')
+            tmplist=os.listdir(tmpdir)
+            filecount=len(tmplist)
 
+            #重新編號命名
+            for i in range(0,filecount):
+                nowfile=tmpdir+tmplist[i]
+                if i<=9:
+                    newname=tmpdir+'0000'+str(i)+'_streaming.wav'
+                else:
+                    newname=tmpdir+'000'+str(i)+'_streaming.wav'
+                os.rename(nowfile,newname)
+                os.system(f'{deretore} {newname}')
+        
+            #刪除轉換前檔案
+            tmplist=os.listdir(tmpdir)
+            for file in tmplist:
+                if file.endswith('.wav'):
+                    os.remove(tmpdir+file)
+            
+            #重新包裝
+            os.system(f'{sat} {tmpdir[:-1]}')
+            shutil.rmtree(tmpdir)
+            
     else:
-        print('[ERROR] path is not exist')
+        print('[ERROR] path not found!')
+        
+
+
 
 if __name__=='__main__':
     path=str(input('path:'))
