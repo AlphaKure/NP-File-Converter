2de2c18f7fbcebdd92cc32295193c2fad7ad006c
Alpha <alpha6662017@outlook.com>
2022-02-17 17:46:44 +0800

    [Chara] 寫檔功能完成，修了一下程式碼

--- chara.py
+++ chara.py
@@ -26,42 +26,57 @@ def chara(path: str):
                 try:
                     with open(nowfile, 'r', encoding='utf-8')as f:
                         data = f.read()
-                        data = BeautifulSoup(data, 'lxml')
+                        data = BeautifulSoup(data, 'xml')
                         f.close()
-                    ranks = data.find('ranks')
-                    for chararank in ranks.find_all('chararankdata'):
-                        skillid=-1
-
-                        #刪除不需要的資料
-                        if not chararank.find('type').string=='1' or int(chararank.find('index').string)==1 :
-                            chararank.decompose()
-                        else:
-
-                            #修改tag名稱
-                            for tags in chararank.find_all('rewardskillseed'):
-                                tags.name='skill'
-                            
-                            #讀取技能名稱並更改
-                            skillname=chararank.skill.skill.str.string
-                            if skillname.endswith('×5'):
-                                skillname=skillname.replace('×5','')
-                            elif skillname.endswith('×1'):
-                                skillname=skillname.replace('×1','') 
-                            if skillname=='Invalid':
-                                skillid=-1
-                            else:
-                                for item in database:
-                                    if skillname==item['name']:
-                                        skillid=item['id']
-                                        break
-                            chararank.skill.skill.id.string=skillid
-                            chararank.skill.skill.str.string=skillname
-                    '''
-                    測試用:
-                    print(data.prettify())
-                    '''
                 except:
                     print(f'[ERROR] {nowfile} can not read!')
+                    break
+                ranks = data.find('ranks')
+                for chararank in ranks.find_all('CharaRankData'):
+                    skillid=-1
+
+                    #刪除不需要的資料
+                    if not chararank.find('type').string=='1' or int(chararank.find('index').string)==1 :
+                        chararank.decompose()
+                    else:
+
+                        #修改tag名稱
+                        for tags in chararank.find_all('rewardSkillSeed'):
+                            tags.name='skill'
+                            
+                        #讀取技能名稱並更改
+                        skillname=chararank.skill.skill.str.string
+                        if skillname.endswith('×5'):
+                            skillname=skillname.replace('×5','')
+                        elif skillname.endswith('×1'):
+                            skillname=skillname.replace('×1','') 
+                        if skillname=='Invalid':
+                            skillid=-1
+                        else:
+                            for item in database:
+                                if skillname==item['name']:
+                                    skillid=item['id']
+                                    break
+                        chararank.skill.skill.id.string=skillid
+                        chararank.skill.skill.str.string=skillname
+                            
+                #新增標籤 firstskill
+                charaskill=data.find('skill').str.string
+                charaskillid=data.find('skill').id.string
+                newtag=BeautifulSoup('<firstSkill><id>'+charaskillid+'</id><str>'+charaskill+'</str><data /></firstSkill>','xml')
+                data.CharaData.defaultHave.insert_after(newtag)
+
+                #寫檔
+                with open(nowfile, 'w', encoding='utf-8')as f:
+                    f.write(data.prettify())
+                    f.close()
+                
+                '''
+                #測試用:
+                print(data.prettify())
+                os.system('PAUSE')
+                '''
+        print('[SUCCESS] All Done!')
     else:
         print('[ERROR] path is not exist')
 
