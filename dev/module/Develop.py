94071ad1ef1d4c1867309911bb45bcf05e84d02e
Alpha <alpha6662017@outlook.com>
2022-05-22 22:39:12 +0800

    Update Develop.py

--- dev/module/Develop.py
+++ dev/module/Develop.py
@@ -1,12 +1,28 @@
-#TEST FOR GET previewTime
+import os
+import ujson
 
-with open('','rb')as f: #<-Ender the file(acb)
-    Data=f.read().hex()
+import tool
+
+lPreviewTime=list()
+
+def PreviewTimeget(musicID,file):
+    global lPreviewTime
+    if tool.read_setting('PreviewTime','GetPreviewTime')=='True':
+        if tool.read_setting('PreviewTime','PreviewTimeSave')=='False':
+            with open(file,'rb')as f:
+                Data=f.read().hex()
+                f.close()
+            lPreviewTime.append({'musicID':musicID,'PrewviewTimeStart':str(int(Data[8236:8242],16)),'PreviewTimeEnd':str(int(Data[8264:8270],16))})
+
+
+if __name__=='__main__':
+    targetdir=input()
+    for dir in os.listdir(targetdir):
+        for file in os.listdir(targetdir+'\\'+dir):
+            if file.endswith('.acb'):
+                target_acb=targetdir+'\\'+dir+'\\'+file
+                PreviewTimeget(file[5:9],target_acb)
+
+with open('TEST.json','w',encoding='utf-8')as f:
+    f.write(ujson.dumps(lPreviewTime))
     f.close()
-
-print(Data[8620:8626]) #previewTimeStart
-print(int(Data[8620:8626],16))
-
-print('##################')
-print(Data[8648:8654]) #previewTimeEnd
-print(int(Data[8648:8654],16))
